70a8a659882ff7c3c70150ac48984e51048327a0
athiraknair743 <athirakookkal@gmail.com>
2025-08-11 13:18:45 +0530

    Update ColorCoder.java

--- colorcoder/ColorCoder.java
+++ colorcoder/ColorCoder.java
@@ -6,13 +6,13 @@ public final class ColorCoder {
 
     public static ColorPair getColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
-        MajorColor majorColor = MajorColor.fromIndex(zeroBasedPairNumber / MinorColor.BLUE.numberOfColors());
-        MinorColor minorColor = MinorColor.fromIndex(zeroBasedPairNumber % MinorColor.BLUE.numberOfColors());
+        MajorColor majorColor = MajorColor.fromIndex(zeroBasedPairNumber / MinorColor.values().length);
+        MinorColor minorColor = MinorColor.fromIndex(zeroBasedPairNumber % MinorColor.values().length);
         return new ColorPair(majorColor, minorColor);
     }
 
     public static int getPairNumberFromColor(MajorColor major, MinorColor minor) {
-        return major.getIndex() * MinorColor.BLUE.numberOfColors() + minor.getIndex() + 1;
+        return major.getIndex() * MinorColor.values().length + minor.getIndex() + 1;
     }
 
     public static void printColorCodeManual() {
